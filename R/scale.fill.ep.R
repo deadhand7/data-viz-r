21c19360da290ceda12ec83620cccb0b53e886ef
Chris Schulz <chris.schulz@europace.de>
2019-10-04 16:05:35 +0200

    desc and format adjusted

--- R/scale.fill.ep.R
+++ R/scale.fill.ep.R
@@ -1,11 +1,11 @@
 #' ep scale fill
 #'
-#' @description This function creates ep formatted, standardized density plots.
+#' @description This function creates applies the ep palette to ggplot graphs
 #' @param palette The color palette.
 #' @param discrete A logical variable. Defaults "FALSE".
 #' @param reverse A logical variable. Defaults "FALSE".
 #' @return ggplot
-#' @keywords density
+#' @keywords scale fill
 #' @import ggplot2
 #' @export
 #' @examples scale.fill.ep(palette = "main")
@@ -13,9 +13,7 @@
 scale.fill.ep <- function(palette = "main", discrete = TRUE, reverse = FALSE, ...) {
   pal <- ep_pal(palette = palette, reverse = reverse)
 
-  if (discrete) {
-    discrete_scale("fill", paste0("ep_", palette), palette = pal, ...)
-  } else {
-    scale_fill_gradientn(colours = pal(256), ...)
-  }
+  if (discrete) {discrete_scale("fill", paste0("ep_", palette), palette = pal, ...)}
+
+  else {scale_fill_gradientn(colours = pal(256), ...)}
 }
